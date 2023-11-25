4c2b1336662edddfe70d4f45c40a33abb4615758
$AM $TARK <sambathnaga5@gmail.com>
2023-11-25 14:30:12 +0530

    updated

--- client3.py
+++ client3.py
@@ -35,25 +35,46 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2 
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b != 0:
+        return price_a / price_b
+    else:
+        return None  
 
 
-# Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+    prices = {}  # Create a dictionary to store stock prices
 
-        """ ----------- Update to get the ratio --------------- """
+    # Query the price once every N seconds.
+    for _ in range(N):
+        try:
+            quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        except Exception as e:
+            print(f"Error fetching quotes: {e}")
+            continue  # Skip the current iteration if there's an error
+
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price  # Store the price in the dictionary
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        # Use the stored prices to calculate and print the ratio
+        stock_a = 'ABC'
+        stock_b = 'DEF'
+
+        price_a = prices.get(stock_a)
+        price_b = prices.get(stock_b)
+
+        if price_a is not None and price_b is not None:
+            ratio = getRatio(price_a, price_b)
+            if ratio is not None:
+                print(f"Ratio {ratio}")
+            else:
+                print("Cannot calculate ratio. Denominator is zero.")
+        else:
+            print(f"Cannot calculate ratio. One or both stock prices not found. Available stocks: {list(prices.keys())}")
